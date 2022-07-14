5587a516a90d517ceb20e681c03f5b46b123b5fd
diazpedrodev <081diazpedro@gmail.com>
2022-07-14 11:42:45 -0300

    atualizando lista de atividades

---
+++ Atividades/atividadesJS/trabalhandoModulos/assets/js/main.mjs
@@ -0,0 +1,5 @@
+import {mostraIdade ,mostraCidade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Pedro", 25));
+console.log(mostraCidade("Pedro", "Recife"));
+console.log(mostraHobby("Pedro", "Treinar"));
