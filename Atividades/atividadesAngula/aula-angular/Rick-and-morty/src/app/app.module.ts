ac77b22861a1ece99f4b423d7be124c56daac68d
diazpedrodev <081diazpedro@gmail.com>
2022-08-03 15:47:49 -0300

    services-api

---
+++ Atividades/atividadesAngula/aula-angular/Rick-and-morty/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ListApiComponent } from './components/list-api/list-api.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListApiComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Atividades/atividadesAngula/aula-angular/Rick-and-morty/src/app/components/list-api/list-api.component.ts
@@ -0,0 +1,26 @@
+import { ListService } from 'src/app/shared/services/list.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-api',
+  templateUrl: './list-api.component.html',
+  styleUrls: ['./list-api.component.scss']
+})
+export class ListApiComponent implements OnInit {
+  personagens: Array<any> = []
+
+  constructor(private listService: ListService) { }
+
+  ngOnInit(): void {
+    this.getList()
+  }
+
+  getList() {
+    this.listService.getList().subscribe(result => {
+      this.personagens = result?.results;
+      
+      console.log(this.personagens)
+    })
+    }
+
+}
